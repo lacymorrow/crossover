12eb124db27d80272a2ebcc510006d2c99d75f97
Lacy Morrow <me@lacymorrow.com>
2019-10-08 11:40:02 -0700

    separate utilities

---
+++ src/util.js
@@ -0,0 +1,12 @@
+// Utilities
+
+const debounce = (func, delay) => {
+	let debounceTimer
+	return function(...args) {
+		const context = this
+		clearTimeout(debounceTimer)
+		debounceTimer = setTimeout(() => func.apply(context, args), delay)
+	}
+}
+
+exports.debounce = debounce
