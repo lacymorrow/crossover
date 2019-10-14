402e49ad5047d5d9d527ce2424b376c90c5dc386
Lacy Morrow <me@lacymorrow.com>
2019-10-14 11:39:50 -0700

    transparent background

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const fs = require('fs')
 
 const path = require('path')
 const {app, ipcMain, globalShortcut, BrowserWindow, Menu} = require('electron')
-// Const {autoUpdater} = require('electron-updater')
+const {autoUpdater} = require('electron-updater')
 const {is} = require('electron-util')
 const unhandled = require('electron-unhandled')
 const debug = require('electron-debug')
-// Const contextMenu = require('electron-context-menu')
+// const contextMenu = require('electron-context-menu')
 const {debounce} = require('./src/util')
 const {config, defaults} = require('./src/config')
 const menu = require('./src/menu')
@@ -21,14 +21,14 @@ app.setAppUserModelId('com.lacymorrow.CrossOver')
 
 // Uncomment this before publishing your first version.
 // It's commented out as it throws an error if there are no published versions.
-// if (!is.development) {
-// 	const FOUR_HOURS = 1000 * 60 * 60 * 4
-// 	setInterval(() => {
-// 		autoUpdater.checkForUpdates()
-// 	}, FOUR_HOURS)
+if (!is.development) {
+	const FOUR_HOURS = 1000 * 60 * 60 * 4
+	setInterval(() => {
+		autoUpdater.checkForUpdates()
+	}, FOUR_HOURS)
 
-// 	autoUpdater.checkForUpdates()
-// }
+	autoUpdater.checkForUpdates()
+}
 
 // Prevent window from being garbage collected
 let mainWindow
