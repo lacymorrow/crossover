fddf1309ec9de5354f7d3abe38d47fc21d0ef67e
Lacy Morrow <me@lacymorrow.com>
2021-01-31 17:00:19 -0800

    travis linux

--- src/main.js
+++ src/main.js
@@ -127,7 +127,7 @@ const createMainWindow = async () => {
 	const win = new BrowserWindow( preferences )
 
 	setDockVisible( false )
-	win.setFullScreenable(false)
+	win.setFullScreenable( false )
 	// VisibleOnFullscreen removed in https://github.com/electron/electron/pull/21706
 	win.setVisibleOnAllWorkspaces( true, { visibleOnFullScreen: true } )
 	// Enables staying on fullscreen apps - mac
