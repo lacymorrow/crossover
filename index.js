955902ab03a758b1bf44b4041fe942c13e60f596
Lacy Morrow <me@lacymorrow.com>
2022-03-25 14:56:09 -0700

    appx

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const process = require( 'process' )
 const { version } = require( './package.json' )
 
 // CLI version
-const args = new Set( process.argv.filter( ( element, i ) => (( i > 0 && i < 4 ) && String( element ).toLowerCase() )) )
+const args = new Set( process.argv.filter( ( element, i ) => ( ( i > 0 && i < 4 ) && String( element ).toLowerCase() ) ) )
 
 if ( args.has( '--version' ) || args.has( '-v' ) ) {
 
@@ -13,24 +13,26 @@ if ( args.has( '--version' ) || args.has( '-v' ) ) {
 
 }
 
-console.log(`CrossOver ${version}`)
+console.log( `CrossOver ${version}` )
 
-if (args.length > 0) {
-	console.log('Arguments: ', args)
+if ( args.length > 0 ) {
+
+	console.log( 'Arguments:', args )
+
 }
 
 if ( args.has( '--debug' ) || args.has( '-d' ) ) {
 
 	console.log( '<Development Mode>' )
-	process.env.NODE_ENV = 'development';
-	process.env.ELECTRON_IS_DEV = 1;
+	process.env.NODE_ENV = 'development'
+	process.env.ELECTRON_IS_DEV = 1
 
 }
 
 if ( args.has( '--reset' ) || args.has( '-r' ) ) {
 
 	console.log( '<Reset App>' )
-	process.env.CROSSOVER_RESET = true;
+	process.env.CROSSOVER_RESET = true
 
 }
 
