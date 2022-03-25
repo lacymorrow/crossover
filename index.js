bd78a5d5a1a28245aaf6f3acb3da0886b176701b
Lacy Morrow <me@lacymorrow.com>
2022-03-24 23:47:11 -0700

    readme

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 /* eslint unicorn/prefer-module: 0 */
 
+const process = require('process');
+
 // CLI version
-const args = process.argv.map((e,i)=>(0 < i && i < 4) && String(e).toLowerCase())
-if (args.includes('--version') || args.includes('-v')) {
-	const version = require('./package.json').version
-	process.stdout.write(version)
+const args = new Set( process.argv.map( ( element, i ) => ( i > 0 && i < 4 ) && String( element ).toLowerCase() ) )
+if ( args.has( '--version' ) || args.has( '-v' ) ) {
+
+	const { version } = require( './package.json' )
+	process.stdout.write( version )
 	process.exit()
+
 }
 
 // App entry
 const main = require( './src/index.js' )
+
 main()
